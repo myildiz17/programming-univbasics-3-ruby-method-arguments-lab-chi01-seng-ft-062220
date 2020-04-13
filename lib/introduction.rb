76f8daccbd3ff1587f08a7cf33b26d16807c55c6
Muhammet Yildiz <muhammetyildiz@gmail.com>
2020-04-13 01:18:17 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -14,6 +14,11 @@ end
 
 introduction_with_language "muhammet", "Ruby"
 
+def introduction_with_language_optional (name , language="Ruby")
+  puts "Hi, my name is #{name} and I am learning to prgram in #{language}."
+end
+
+introduction_with_language_optional "Muhammet"
 
 
 
